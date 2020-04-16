a8abe5c5a8114cf458acb670739bbdb057767196
DR-DRU <negoescu@gmx.de>
2020-04-16 05:11:15 +0200

    Basic Arrow

--- Assets/Stage 2/Scripts/Arrow.cs
+++ Assets/Stage 2/Scripts/Arrow.cs
@@ -2,41 +2,49 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Projectile : MonoBehaviour
+public class Arrow : MonoBehaviour
 {
     public float projectileSpeed;
     public int damage;
-    
+
 
     // Start is called before the first frame update
     void Start()
     {
         Destroy(gameObject, 5.0f);
-
+        GetComponent<Rigidbody>().velocity = transform.forward * 40;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += transform.forward * Time.deltaTime * projectileSpeed;
+
     }
 
     void OnCollisionEnter(Collision collision)
     {
-        if(collision.gameObject.GetComponent<Enemy>() != null)
+        if (collision.gameObject.GetComponent<Enemy>() != null)
         {
             collision.gameObject.GetComponent<Enemy>().health -= damage;
             Destroy(gameObject);
         }
         else if (collision.gameObject.GetComponent<HealthAndRespawn>() != null)
         {
-            
+
         }
         else
         {
             Destroy(gameObject);
         }
-        
+
     }
 
+    void ArrowTestFunction()
+    {
+        Vector3 vel = GetComponent<Rigidbody>().velocity;
+        float angle = Mathf.Atan2(vel.y, vel.x) * Mathf.Rad2Deg;
+
+        print(angle);
+        transform.eulerAngles = new Vector3(0, 0, angle);
+    }
 }
