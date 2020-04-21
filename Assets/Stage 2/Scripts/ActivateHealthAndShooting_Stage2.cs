289d3ae4f3ace884582fbd851bab49184d5d80a1
DR-DRU <negoescu@gmx.de>
2020-04-21 22:20:43 +0200

    Updated scripts for stage 2

---
+++ Assets/Stage 2/Scripts/ActivateHealthAndShooting_Stage2.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ActivateHealthAndShooting_Stage2 : MonoBehaviour
+{
+    public Shooting_Stage2 shooting;
+    public Text healthCounter;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.name.Equals("Player"))
+        {
+            shooting.enabled = true;
+            healthCounter.enabled = true;
+        }
+    }
+}
