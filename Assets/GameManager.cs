c10fd274532b945012657361d6d87bd2f9de7d49
DR-DRU <negoescu@gmx.de>
2020-04-24 02:17:40 +0200

    Preliminary Pause menu

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     public static bool devNoteCurrentlyDisplayed = false;
+    public static bool gamePaused = false;
+    public Canvas pauseMenu;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,27 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetButtonDown("Pause"))
+        {
+            TogglePause();
+        }
+
+    }
+
+    void TogglePause()
+    {
+        if (gamePaused)
+        {
+            pauseMenu.enabled = false;
+            gamePaused = false;
+            Time.timeScale = 1;
+        }
+        else
+        {
+            pauseMenu.enabled = true;
+            gamePaused = true;
+            Time.timeScale = 0;
+        }
     }
 }
