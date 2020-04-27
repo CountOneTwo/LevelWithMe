d75af89fef731ae083a26daadbc396428025ec67
DR-DRU <negoescu@gmx.de>
2020-04-27 03:06:04 +0200

    Revert "Updated health"
    This reverts commit ee83cd7a55079d2ee11500a8b19637726bca6ea3.

--- Assets/Stage 0 (Test Scene)/Scripts/EnemyProjectile.cs
+++ Assets/Stage 0 (Test Scene)/Scripts/EnemyProjectile.cs
@@ -22,9 +22,9 @@ public class EnemyProjectile : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {      
-         if (collision.gameObject.GetComponent<HealthAndRespawn_Stage2>() != null)
+         if (collision.gameObject.GetComponent<HealthAndRespawn>() != null)
         {
-            collision.gameObject.GetComponent<HealthAndRespawn_Stage2>().currentHealth -= damage;
+            collision.gameObject.GetComponent<HealthAndRespawn>().currentHealth -= damage;
             Destroy(gameObject);
         }
         else if (collision.gameObject.GetComponent<Enemy>() != null)
