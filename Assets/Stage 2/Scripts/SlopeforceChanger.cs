d68165eb9db255d117b039888a27b322c3eb1ccd
Moonydev <ignaciodipaola@hotmail.com>
2020-05-06 11:49:42 +0200

    Tune enemy positions, Devnotes positions + content, Checkpoints

---
+++ Assets/Stage 2/Scripts/SlopeforceChanger.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlopeforceChanger : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<Moving_Stage2>() != null)
+        {
+           // collision.gameObject.GetComponent<Moving_Stage2>().currentHealth -= damage;
+            
+        }
+    }
+}
